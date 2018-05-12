8b8e85abb0241fdfef65b340a89eb3a4011c96e9
Jacob Sayles <jsayles@gmail.com>
2018-05-12 12:37:34 -0700

    optionally install libraries

--- install.sh
+++ install.sh
@@ -3,14 +3,15 @@
 SRC=/Volumes/CIRCUITPY
 LIB=$SRC/lib
 
-echo "Uploading library files..."
-mkdir -p $LIB
-cp lib/adafruit_bno055.mpy $LIB
-cp lib/neopixel.mpy $LIB
-cp -r lib/adafruit_bus_device $LIB
-cp -r lib/adafruit_register $LIB
-#cp -r lib/adafruit_hid $LIB
-
+if [ "$1" == "-l" ]; then
+  echo "Uploading library files..."
+  mkdir -p $LIB
+  cp lib/adafruit_bno055.mpy $LIB
+  cp lib/neopixel.mpy $LIB
+  cp -r lib/adafruit_bus_device $LIB
+  cp -r lib/adafruit_register $LIB
+  #cp -r lib/adafruit_hid $LIB
+fi
 
 echo "Uploading the source files..."
 cd src
