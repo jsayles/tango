954d867905bd435cc9c036df46d86355d13506a9
Jacob Sayles <jsayles@gmail.com>
2018-05-13 09:00:25 -0700

    clean up hidden files and sync when done

--- install.sh
+++ install.sh
@@ -3,18 +3,29 @@
 SRC=/Volumes/CIRCUITPY
 LIB=$SRC/lib
 
-if [ "$1" == "-l" ]; then
+if [ "$1" == "-f" ]; then
+  echo "Cleaning up hidden files..."
+  mdutil -i off $SRC > /dev/null
+  cd $SRC > /dev/null
+  rm -rf .{,_.}{fseventsd,Spotlight-V*,Trashes}
+  mkdir .fseventsd
+  touch .fseventsd/no_log .metadata_never_index .Trashes
+  cd - > /dev/null
+
   echo "Uploading library files..."
   mkdir -p $LIB
-  cp lib/adafruit_bno055.mpy $LIB
-  cp lib/neopixel.mpy $LIB
-  cp -r lib/adafruit_bus_device $LIB
-  cp -r lib/adafruit_register $LIB
-  #cp -r lib/adafruit_hid $LIB
+  cp -X lib/adafruit_bno055.mpy $LIB
+  cp -X lib/neopixel.mpy $LIB
+  cp -rX lib/adafruit_bus_device $LIB
+  cp -rX lib/adafruit_register $LIB
+  #cp -rX lib/adafruit_hid $LIB
 fi
 
 echo "Uploading the source files..."
 cd src
-cp core.py $SRC
-cp main.py $SRC
+cp -X core.py $SRC
+cp -X main.py $SRC
 cd - > /dev/null
+
+echo "Syncing filesystems..."
+sync
